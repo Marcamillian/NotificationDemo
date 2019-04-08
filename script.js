07f5c55119d9cd26ce12adcb75081a3a9ab52e14
Marcamillian <marcamillian@gmail.com>
2019-04-08 22:14:33 +0100

    feat: Refactor notification
    script.js
    - Refactor notification creation to spawnNotification function
    - Add body and icon to notification

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 function notifyMe(){
 
+  const notifyContents = {
+    icon:"./notify-icon.png",
+    title:"Hello There",
+    body:"Thankyou for looking at this message"
+  }
+
   if(!("Notification" in window)){ // are notifications supported
     alert("This browser does not support desktop notifications")
   }
   else if (Notification.permission === "granted"){  // do we already have permission
     // we can create a new one
-    var notification = new Notification("Hi there!");
+    spawnNotification( notifyContents )
   }else if (Notification.permission !== "denied"){  // if they haven't been denied - ask
     Notification.requestPermission()
     .then( function(permission){
       if(permission === "granted"){ // if they said yes when we asked
-        var notification = new Notification ("Hi there!");
+        spawnNotification( notifyContents )
       }
     })
   }
+}
+
+function spawnNotification({body, icon, title}){
+  var n = new Notification(title, {icon, body})
 }
