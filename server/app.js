fd5ccc4145a8895aed2e647e3953d332105f7f98
Marcamillian <marcamillian@gmail.com>
2019-04-11 21:54:55 +0100

    feat: Set up express server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,18 @@
 // !!TODO set up the server stuff from this part in the tutorial - https://developers.google.com/web/fundamentals/push-notifications/subscribing-a-user#send_a_subscription_to_your_server
 
 console.log("Something Server-y")
+
+const express = require('express');
+const http = require('http');
+
+
+let app = express();
+const serveDir = './notify-push';
+let server;
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(serveDir));
+
+server = http.createServer(app);
+
+server.listen(app.get('port'), ()=>{ console.log(`listening on port ${app.get('port')}`)})
